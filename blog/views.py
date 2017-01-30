fd1b63d2ae1dce6549f3e77bd1ecf5cb74644fbb
samdasoxide <samdasoxide@gmail.com>
2017-01-30 17:12:15 +0300

    Adding bootstrap and fixing post_detail url

--- blog/views.py
+++ blog/views.py
@@ -2,24 +2,24 @@ from django.shortcuts import render, get_object_or_404
 from .models import Post
 
 
-def Post_list(request):
+def post_list(request):
     """
     Display all publised posts
     Post list view
     """
-    posts = Post.published.all()
+    posts = Post.objects.all()
     return render(request,
                   'blog/post/list.html',
                   {'posts': posts})
 
 
-def Post_detail(request, year, month, day, post):
+def post_detail(request, year, month, day, post):
     """
     View to display a single post
     Post detail view
     """
     post = get_object_or_404(Post, slug=post,
-                             status='publised',
+                             status='published',
                              publish__year=year,
                              publish__month=month,
                              publish__day=day)
